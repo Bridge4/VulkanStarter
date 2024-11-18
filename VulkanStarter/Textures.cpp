a754dab141531baaa91b56718acd7051506005c2
Sayed Abdulmohsen Alhashemi <s.abdulalhash@gmail.com>
2024-11-18 19:13:57 +0300

    Time for fork

--- VulkanStarter/Textures.cpp
+++ VulkanStarter/Textures.cpp
@@ -1,82 +1,82 @@
-#include "Textures.hpp"
-
-
-void createBuffer() {
-
-}
-
-void createImage(std::string texturePath) {
-    int texWidth, texHeight, texChannels;
-
-    stbi_uc* pixels = stbi_load(texturePath.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
-
-    // The multiplication by 4 here is because the pixels are loaded row by row with 4 bytes per pixel
-    VkDeviceSize imageSize = texWidth * texHeight * 4;
-
-    if (!pixels) {
-        throw std::runtime_error("failed to load texture image!");
-    }
-
-    VkBuffer stagingBuffer;
-    VkDeviceMemory stagingBufferMemory;
-
-    createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-        stagingBuffer, stagingBufferMemory);
-
-    void* data;
-    vkMapMemory(m_logical_device, stagingBufferMemory, 0, imageSize, 0, &data);
-    memcpy(data, pixels, static_cast<size_t>(imageSize));
-    vkUnmapMemory(m_logical_device, stagingBufferMemory);
-
-    stbi_image_free(pixels);
-
-    createImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL,
-        VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
-        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_textureImage, m_textureImageMemory);
-
-    transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-    copyBufferToImage(stagingBuffer, m_textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
-    transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-
-    vkDestroyBuffer(m_logical_device, stagingBuffer, nullptr);
-    vkFreeMemory(m_logical_device, stagingBufferMemory, nullptr);
-}
-
-// TEXTURE IMAGE VIEW
-void createImageView() {
-    m_textureImageView = imageView.create(m_logical_device, m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT);
-}
-
-// TEXTURE SAMPLER
-void createSampler() {
-    VkSamplerCreateInfo samplerInfo{};
-    samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-    samplerInfo.magFilter = VK_FILTER_LINEAR;
-    samplerInfo.minFilter = VK_FILTER_LINEAR;
-    samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-    samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-    samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-
-    samplerInfo.anisotropyEnable = VK_TRUE;
-
-    VkPhysicalDeviceProperties properties{};
-    vkGetPhysicalDeviceProperties(m_physicalDevice, &properties);
-    samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
-
-    samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
-    samplerInfo.unnormalizedCoordinates = VK_FALSE;
-
-    samplerInfo.compareEnable = VK_FALSE;
-    samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
-
-    samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-    samplerInfo.mipLodBias = 0.0f;
-    samplerInfo.minLod = 0.0f;
-    samplerInfo.maxLod = 0.0f;
-
-    if (vkCreateSampler(m_logical_device, &samplerInfo, nullptr, &m_textureSampler) != VK_SUCCESS) {
-        throw std::runtime_error("failed to create texture sampler!");
-    }
-}
-
+//#include "Textures.hpp"
+//
+//
+//void createBuffer() {
+//
+//}
+//
+//void createImage(std::string texturePath) {
+//    int texWidth, texHeight, texChannels;
+//
+//    stbi_uc* pixels = stbi_load(texturePath.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+//
+//    // The multiplication by 4 here is because the pixels are loaded row by row with 4 bytes per pixel
+//    VkDeviceSize imageSize = texWidth * texHeight * 4;
+//
+//    if (!pixels) {
+//        throw std::runtime_error("failed to load texture image!");
+//    }
+//
+//    VkBuffer stagingBuffer;
+//    VkDeviceMemory stagingBufferMemory;
+//
+//    createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+//        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+//        stagingBuffer, stagingBufferMemory);
+//
+//    void* data;
+//    vkMapMemory(m_logical_device, stagingBufferMemory, 0, imageSize, 0, &data);
+//    memcpy(data, pixels, static_cast<size_t>(imageSize));
+//    vkUnmapMemory(m_logical_device, stagingBufferMemory);
+//
+//    stbi_image_free(pixels);
+//
+//    createImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL,
+//        VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+//        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_textureImage, m_textureImageMemory);
+//
+//    transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+//    copyBufferToImage(stagingBuffer, m_textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
+//    transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+//
+//    vkDestroyBuffer(m_logical_device, stagingBuffer, nullptr);
+//    vkFreeMemory(m_logical_device, stagingBufferMemory, nullptr);
+//}
+//
+//// TEXTURE IMAGE VIEW
+//void createImageView() {
+//    m_textureImageView = imageView.create(m_logical_device, m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT);
+//}
+//
+//// TEXTURE SAMPLER
+//void createSampler() {
+//    VkSamplerCreateInfo samplerInfo{};
+//    samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+//    samplerInfo.magFilter = VK_FILTER_LINEAR;
+//    samplerInfo.minFilter = VK_FILTER_LINEAR;
+//    samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+//    samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+//    samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+//
+//    samplerInfo.anisotropyEnable = VK_TRUE;
+//
+//    VkPhysicalDeviceProperties properties{};
+//    vkGetPhysicalDeviceProperties(m_physicalDevice, &properties);
+//    samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+//
+//    samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+//    samplerInfo.unnormalizedCoordinates = VK_FALSE;
+//
+//    samplerInfo.compareEnable = VK_FALSE;
+//    samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+//
+//    samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+//    samplerInfo.mipLodBias = 0.0f;
+//    samplerInfo.minLod = 0.0f;
+//    samplerInfo.maxLod = 0.0f;
+//
+//    if (vkCreateSampler(m_logical_device, &samplerInfo, nullptr, &m_textureSampler) != VK_SUCCESS) {
+//        throw std::runtime_error("failed to create texture sampler!");
+//    }
+//}
+//
--- VulkanStarter/src.cpp
+++ VulkanStarter/src.cpp
@@ -44,7 +44,7 @@
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
-const std::string MODEL_PATH = "models/viking_room.obj";
+const std::string MODEL_PATH = "models/viking.obj";
 const std::string TEXTURE_PATH = "textures/viking_room.png";
 
 const int MAX_FRAMES_IN_FLIGHT = 2;
